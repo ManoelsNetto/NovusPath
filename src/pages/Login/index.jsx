44b13e1f3f7c7f90aeb149472e6c8be2b16c758d
Sir Manoel <manoel.silva.neto2013@gmail.com>
2025-11-23 00:56:53 -0300

    Primeiro commit: MVP Global Solution 2025 - V5

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -107,7 +107,7 @@ export default function Login() {
             )}
           </button>
 
-          <p className="text-center text-danger small mb-0">
+          <p className="text-center text-danger small mb-0 fw-bold">
 
             Não precisa de cadastro - apenas entre com qualquer email e senha!
 
